997c713595d7b235097eacb9e16e14d1515ad79a
mnshrt <mnshrt2292@gmail.com>
2020-09-28 01:40:52 +0530

    exercise 1.2

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ return (
 <h1>{course}</h1>
 )
 }
-const Content =({part,exercises})=>{
+const Content =({part1, part2, part3, exercises1, exercises2, exercises3})=>{
   return (
-    <p>
-    {part} {exercises}
-    </p>
+    <div>
+       <Part part={part1} exercises={exercises1}/>
+       <Part part={part2} exercises={exercises2}/>
+       <Part part={part3} exercises={exercises3}/>
+    </div> 
+  )
+}
+const Part = ({part, exercises}) => {
+  return (
+  <p>{part} {exercises}</p>
   )
 }
 
@@ -31,9 +38,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2}exercises3={exercises3}/>
+      
       <Total exercises1 exercises2 exercises3 />
     </div>
   )
